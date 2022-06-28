9df0d3d2169ab902535d24022e78014b4f6fb8ed
Blip blop <OtherHorizon1a@gmail.com>
2022-06-28 13:54:12 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const App = () => (
+  <>
+    <h1>Welcome to React Vite Micro App!</h1>
+    <p>Hard to get more minimal than this React app.</p>
+    <h1>This is a heading for React</h1>
+  </>
+)
+export default App
